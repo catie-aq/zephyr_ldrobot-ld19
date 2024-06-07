f4e0aa7051c7e351c637e6198c4c905ea0194a90
Clément Laigle <c.laigle@catie.fr>
2024-06-07 15:22:44 +0000

    Implement LD19 sensor data parsing

--- drivers/sensor/ld19/ld19.c
+++ drivers/sensor/ld19/ld19.c
@@ -13,15 +13,104 @@
 
 LOG_MODULE_REGISTER(LD19, CONFIG_SENSOR_LOG_LEVEL);
 
+#define LD19_MAX_BUFFER_SIZE      128
+#define LD19_PKG_HEADER           0x54
+#define LD19_DATA_PKG_INFO        0x2C
+#define LD19_HEALTH_PKG_INFO      0xE0
+#define LD19_MANUFACT_PKG_INF     0x0F
+#define LD19_DATA_LEN             12
+#define LD19_DATA_HEALTH_LEN      12
+#define LD19_DATA_MANUFACTURE_LEN 12
+
+enum ld19_state {
+	HEADER,
+	VER_LEN,
+	DATA,
+	DATA_HEALTH,
+	DATA_MANUFACTURE,
+};
+
 struct ld19_config {
 	const struct device *uart_dev;
 };
 
+struct ld19_buf {
+	uint8_t buf[LD19_MAX_BUFFER_SIZE];
+	uint8_t index;
+};
+
 struct ld19_data {
+	struct ld19_buf rx_buf;
+	enum ld19_state state;
 };
 
 static void ld19_uart_callback_handler(const struct device *dev, void *user_data)
 {
+	struct ld19_data *data = ((struct device *)user_data)->data;
+	int len = 0;
+	uint8_t c;
+
+	if ((uart_irq_update(dev) & UART_IRQ_RX_RDY) == 0) {
+		return;
+	}
+
+	while (uart_irq_rx_ready(dev)) {
+		len = uart_fifo_read(dev, &c, 1);
+		if (len < 0) {
+			LOG_ERR("Failed to read data from UART");
+			return;
+		}
+
+		swicth(state)
+		{
+		case HEADER:
+			if (c == LD19_PKG_HEADER) {
+				data->rx_buf.index = 0;
+				data->rx_buf.buf[data->rx_buf.index++] = c;
+				data->state = VER_LEN;
+			}
+			break;
+		case VER_LEN: {
+			if (c == LD19_DATA_PKG_INFO) {
+				data->rx_buf.buf[data->rx_buf.index++] = c;
+				data->state = DATA;
+			} else if (c == LD19_HEALTH_PKG_INFO) {
+				data->rx_buf.buf[data->rx_buf.index++] = c;
+				data->state = DATA_HEALTH;
+			} else if (c == LD19_MANUFACT_PKG_INF) {
+				data->rx_buf.buf[data->rx_buf.index++] = c;
+				data->state = DATA_MANUFACTURE;
+			} else {
+				data->state = HEADER;
+			}
+			break;
+		}
+		case DATA:
+			data->rx_buf.buf[data->rx_buf.index++] = c;
+			if (data->rx_buf.index == LD19_POINT_PER_PACK) {
+				// TODO: CRC check
+				data->state = HEADER;
+			}
+			break;
+		case DATA_HEALTH:
+			data->rx_buf.buf[data->rx_buf.index++] = c;
+			if (data->rx_buf.index == LD19_DATA_HEALTH_LEN) {
+				// TODO: CRC check
+				data->state = HEADER;
+			}
+			break;
+		case DATA_MANUFACTURE:
+			data->rx_buf.buf[data->rx_buf.index++] = c;
+			if (data->rx_buf.index == LD19_DATA_MANUFACTURE_LEN) {
+				// TODO: CRC check
+				data->state = HEADER;
+			}
+			break;
+		default:
+			data->state = HEADER;
+			break;
+		}
+	}
 }
 
 static int ld19_attr_set(const struct device *dev, enum sensor_channel chan,
